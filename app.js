193a89b5351f8b52446b7b06a4fd1db803b1e916
ali32fps-cloud <239955998+alimohammadkarami@users.noreply.github.com>
2026-01-03 21:40:38 +0330

    cors fixed

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express();
+const cors = require("cors")
 const axios = require('axios')
 const currencies_router = require('./routes/currencies')
 const Gold_router = require('./routes/gold');
 const crypto_router = require('./routes/crypto')
 require('dotenv').config();
 app.use(express.json());
+app.use(cors());
 const port = process.env.APP_PORT || 3000;
 app.listen(port,()=>{
  console.log(`server running on  ${port}`)
