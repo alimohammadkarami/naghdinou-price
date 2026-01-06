3a38fb1122a5c6e19e2183fef226bd7161b72ff2
ali32fps-cloud <239955998+alimohammadkarami@users.noreply.github.com>
2026-01-06 14:57:13 +0330

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const crypto_router = require('./routes/crypto')
 require('dotenv').config();
 app.use(express.json());
 app.use(cors());
-const port = process.env.APP_PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port,()=>{
  console.log(`server running on  ${port}`)
 })
